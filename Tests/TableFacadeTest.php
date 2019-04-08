7d53ec1cb7207711e39eade3f8f0c82d96fc85af
Benoit Michaud <bendamqui@icloud.com>
2019-04-08 01:35:26 -0400

    Initial commit

---
+++ Tests/TableFacadeTest.php
@@ -0,0 +1,127 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Bendamqui\DbUnit\TableFacade;
+use PHPUnit\DbUnit\DataSet\ArrayDataSet;
+
+class TableFacadeTest extends TestCase
+{
+    const ROW_COUNT = 5;
+
+    /**
+     * @var TableFacade
+     */
+    private $table;
+
+    /**
+     * Use a random number for the row to fetch when fetching one row or
+     * another is irrelevant.
+     *
+     * @var int
+     */
+    private $row_number;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->table = new TableFacade($this->getDataSet()->getTable('users'));
+        $this->row_number = rand(0, self::ROW_COUNT - 1);
+
+    }
+
+    public function getDataSet()
+    {
+        $data = file_get_contents(__DIR__.'/../Tests/fixture.json');
+        $data = json_decode($data, true);
+        return new ArrayDataSet($data);
+    }
+
+    public function testGetAllRaw()
+    {
+        $result = $this->table->getAllRaw();
+        $this->assertCount(self::ROW_COUNT, $result);
+    }
+
+    public function testGetRaw()
+    {
+        $result = $this->table->getRaw();
+        $this->assertEquals(1, $result['id']);
+    }
+
+    public function testGetRawByRow()
+    {
+        $result = $this->table->getRaw($this->row_number);
+        $this->assertEquals($this->row_number + 1, $result['id']);
+    }
+
+    public function testGetRowCount()
+    {
+        $result = $this->table->getRowCount();
+        $this->assertEquals(self::ROW_COUNT, $result);
+    }
+
+    public function testGetValue()
+    {
+        $result = $this->table->getValue('first_name', 3);
+        $this->assertEquals('Kianna', $result);
+    }
+
+    public function testGetFirst()
+    {
+        $result = $this->table->get();
+        $this->assertEquals(1, $result['id']);
+    }
+
+    public function testGetByRow()
+    {
+        $result = $this->table->get([], $this->row_number);
+        $this->assertEquals($this->row_number + 1, $result['id']);
+    }
+
+    public function testGetOverride()
+    {
+        $result = $this->table->get(['first_name' => 'Bob'], $this->row_number);
+        $this->assertEquals('Bob', $result['first_name']);
+    }
+
+    /**
+     * @param array $filters
+     * @param int $expected_row_count
+     * @dataProvider filterProvider
+     */
+    public function testGetWhere($filters, $expected_row_count)
+    {
+        $result = $this->table->getWhere($filters);
+        $this->assertIsArray($result);
+        $this->assertCount($expected_row_count, $result);
+        foreach ($filters as $key => $value) {
+            foreach ($result as $row) {
+                $this->assertEquals($value, $row[$key]);
+            }
+        }
+
+    }
+
+    public function testDotSetter()
+    {
+        $result = $this->table->get(['a.b' => 1]);
+        $this->assertEquals(1, $result['a']['b']);
+    }
+
+    /**
+     * - Filters
+     * - Expected row count
+     *
+     * @return array
+     */
+    public function filterProvider()
+    {
+        return [
+            [['id' => 1], 1],
+            [['id' => 2, 'created_at' => '2019-04-06 03:56:44'], 1],
+            [['created_at' => '2019-04-06 03:56:44'], 5],
+            [['first_name' => 'Bob'], 0],
+            [['first_name' => 'Bob', 'created_at' => '2019-04-06 03:56:44'], 0],
+        ];
+    }
+}
---
+++ src/TableFacade.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace Bendamqui\DbUnit;
+
+use PHPUnit\DbUnit\DataSet\ITable;
+
+/**
+ * Use the PHPUnit\DbUnit\DataSet\ITable to get mock payload based on the data
+ * fixture set in DbUnit without querying the database.
+ *
+ * Class BaseFixtureAdaptor
+ */
+class TableFacade
+{
+    /**
+     * @var ITable
+     */
+    private $table;
+
+    /**
+     * BaseFixtureAdaptor constructor.
+     *
+     * @param ITable $table
+     */
+    public function __construct(ITable $table)
+    {
+        $this->table = $table;
+    }
+
+    /**
+     * Get all rows in raw format (skip post processing).
+     *
+     * @return array
+     */
+    public function getAllRaw()
+    {
+        $output = [];
+        $row_count = $this->getRowCount();
+
+        for ($i = 0; $i < $row_count; $i++) {
+            $output[] = $this->getRaw($i);
+        }
+
+        return $output;
+    }
+
+    /**
+     * Get a row by its row number (skip post processing).
+     *
+     * @param int $row
+     * @return array
+     */
+    public function getRaw($row = 0)
+    {
+        return $this->table->getRow($row);
+    }
+
+    /**
+     * @param array $payload
+     * @param array $override
+     *
+     * @return array
+     */
+    private function process($payload, $override)
+    {
+        $payload = $this->postProcess($payload);
+        foreach ($override as $key => $value) {
+            $payload = $this->dotSetter($payload, $key, $value);
+        }
+
+        return $payload;
+    }
+
+    /**
+     * Takes a raw fixture from the table and override the properties
+     * passed in $override.
+     *
+     * @param array $override
+     * @param int   $row
+     *
+     * @return array
+     */
+    public function get($override = [], $row = 0)
+    {
+        $payload = $this->getRaw($row);
+        return $this->process($payload, $override);
+    }
+
+    /**
+     * Get the value of a given row/column in the Table.
+     *
+     * @param $column
+     * @param int $row
+     *
+     * @return mixed
+     */
+    public function getValue($column, $row = 0)
+    {
+        return $this->table->getValue($row, $column);
+    }
+
+    /**
+     * Perform a count on the array that populated the DB in DbUnit set up
+     * so we don't need to query the DB to get an initial row count.
+     *
+     * @return int
+     */
+    public function getRowCount()
+    {
+        return $this->table->getRowCount();
+    }
+
+    /**
+     * @param array  $payload
+     * @param string $keys
+     * @param mixed  $value
+     *
+     * @return array
+     */
+    private function dotSetter($payload, $keys, $value)
+    {
+        $copy = &$payload;
+        $keys = explode('.', $keys);
+
+        foreach ($keys as $key) {
+            if (is_array($copy)) {
+                $copy = &$copy[$key] ?? null;
+            } elseif (is_object($copy)) {
+                $copy = &$copy->$key ?? null;
+            } else {
+                $copy = [];
+                $copy = &$copy[$key];
+            }
+        }
+
+        $copy = $value;
+
+        return $payload;
+    }
+
+    /**
+     * Get many rows using filters in the form of key value. Perform AND only.
+     *
+     * @param array $filters
+     *
+     * @return array
+     */
+    public function getWhere($filters = [])
+    {
+        $output = [];
+        $fixtures = $this->getAllRaw();
+        foreach ($fixtures as $fixture) {
+            $found = true;
+            foreach ($filters as $key => $value) {
+                if ($fixture[$key] != $value) {
+                    $found = false;
+                }
+            }
+            if ($found) {
+                $output[] = $fixture;
+            }
+        }
+
+        return $output;
+    }
+
+    /**
+     * Placeholder, can be override in order to process the raw data in the db to
+     * the actual format of the payload. e.g. The db store a name while the web form
+     * send first_name and last_name separately.
+     *
+     * @param $payload
+     * @return mixed
+     */
+    protected function postProcess($payload)
+    {
+        return $payload;
+    }
+}
