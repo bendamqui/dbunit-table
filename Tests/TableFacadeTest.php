c7765b4ef7ea9b58a20260885fdf9d6e15c79f91
bendamqui <bendamqui@icloud.com>
2019-11-09 12:57:00 -0500

    #31 Add get all method

--- Tests/TableFacadeTest.php
+++ Tests/TableFacadeTest.php
@@ -176,6 +176,19 @@ class TableFacadeTest extends TestCase
         $this->assertEquals(array_fill(0, 3, 'admin'), $result['role']);
     }
 
+    public function testGetAll()
+    {
+        $result = $this->table->getAll();
+        $this->assertCount(self::ROW_COUNT, $result);
+    }
+
+    public function testGetAllWithOverride()
+    {
+        $result = $this->table->getAll(['email' => 'override_email']);
+        $this->assertCount(self::ROW_COUNT, $result);
+        $this->assertEquals(array_unique(array_column($result, 'email'))[0], 'override_email');
+    }
+
     /**
      * - Filters
      * - Expected row count.
